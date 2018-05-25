80215e32f19eb62c9074589c6ae4347a0fd06a03
Alex Odi <alexodintsov@mbp-alex.dlink>
2018-05-25 22:21:50 +0300

    Initial Commit

---
+++ h.h.i.p/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  h.h.i.p
+//
+//  Created by Alex Odintsov on 25.05.2018.
+//  Copyright © 2018 Alex Odintsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
